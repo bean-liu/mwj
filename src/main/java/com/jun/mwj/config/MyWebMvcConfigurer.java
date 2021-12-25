8adc6082ef1d5b88efdc3d1eb42e9fd9e14febbf
bean-liu <529519366@qq.com>
2021-12-25 17:01:45 +0800

    前端实现登录拦截

--- src/main/java/com/jun/mwj/config/MyWebMvcConfigurer.java
+++ src/main/java/com/jun/mwj/config/MyWebMvcConfigurer.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     /*@Override
